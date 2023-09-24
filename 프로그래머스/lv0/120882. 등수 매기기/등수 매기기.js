afe4835ad694276b5eefcee761c9d7774eb0d06d
GoJiMin <112407281+GoJiMin@users.noreply.github.com>
2023-09-24 22:00:41 +0900

    [level 0] Title: 등수 매기기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.js
@@ -0,0 +1,7 @@
+const solution = score => {
+    const avg = score.map(v => (v[0] + v[1]) /2)
+    
+    const sorted = [...avg].sort((a, b) => b - a)
+    
+    return avg.map(v => sorted.indexOf(v) + 1)
+}
