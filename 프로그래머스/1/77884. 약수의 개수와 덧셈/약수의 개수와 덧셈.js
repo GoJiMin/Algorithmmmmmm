29b95e8d221c95b421d475b26247ff9b3bd103ce
GoJiMin <112407281+GoJiMin@users.noreply.github.com>
2023-10-23 23:56:56 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.51 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.js
@@ -0,0 +1,19 @@
+const solution = (left, right) => {
+    let result = 0;
+    
+    for(let i = left; i <= right; i++) {
+        let arr = []
+        for(let j = 1; j <= i; j++) {
+            if(i % j === 0) {
+                arr.push(j)
+            }
+        }
+        if(arr.length % 2 === 0) {
+            result += i
+        } else {
+            result -= i
+        }
+    }
+    
+    return result
+}
