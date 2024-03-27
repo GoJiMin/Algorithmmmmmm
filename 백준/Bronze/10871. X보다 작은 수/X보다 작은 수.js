176eb7cb2d18318f1eaedd9d963d8fc76d16ce96
Prolip <gojimin3095@gmail.com>
2024-03-27 22:52:13 +0900

    [Bronze V] Title: X보다 작은 수, Time: 132 ms, Memory: 10732 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -1,11 +1,6 @@
-const fs = require('fs');
-const input = fs.readFileSync("/dev/stdin").toString().split("\n").map(el => el.split(" ").map(Number));
+const input = require('fs').readFileSync("/dev/stdin").toString().trim().split("\n").map(el => el.split(" "))
 
 const [n, x] = input[0]
 const arr = input[1]
 
-function solution(n, x, arr) {
-    arr.map((num) => num < x && console.log(num));
-}
-
-solution(n, x, arr)
+console.log(arr.filter(num => +num < +x).join(" "))
