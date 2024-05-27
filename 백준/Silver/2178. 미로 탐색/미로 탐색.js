86b34ed1028ac9750a52982dde95c6124ebb1c7c
Prolip <gojimin3095@gmail.com>
2024-05-27 21:01:58 +0900

    [Silver I] Title: 미로 탐색, Time: 204 ms, Memory: 12476 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.js
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.js
@@ -4,8 +4,6 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-const [nm, ...arr] = input;
-
 class Node {
   constructor(data) {
     this.data = data;
@@ -50,32 +48,40 @@ class Queue {
   }
 }
 
+const [nm, ...arr] = input;
 const [n, m] = nm.split(" ").map(Number);
-const maze = arr.map((el) => el.trim().split(""));
-const vis = Array.from(Array(n), () => Array(m).fill(-1));
+const boards = arr.map((el) => el.trim().split(""));
 
+const dis = Array.from(Array(n), () => Array(m).fill(-1));
 const dx = [0, 0, -1, 1];
 const dy = [1, -1, 0, 0];
+
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    if (boards[i][j] === "0") {
+      dis[i][j] = 0;
+    }
+  }
+}
+
 const queue = new Queue();
 
-vis[0][0] = 1;
+dis[0][0] = 1;
 queue.push([0, 0]);
 
 while (!queue.empty()) {
   const [x, y] = queue.pop();
-  const dis = vis[x][y];
 
   for (let dir = 0; dir < 4; dir++) {
     const nx = x + dx[dir];
     const ny = y + dy[dir];
 
     if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+    if (dis[nx][ny] > -1) continue;
 
-    if (vis[nx][ny] < 0 && maze[nx][ny] === "1") {
-      queue.push([nx, ny]);
-      vis[nx][ny] = dis + 1;
-    }
+    queue.push([nx, ny]);
+    dis[nx][ny] = dis[x][y] + 1;
   }
 }
 
-console.log(vis[n - 1][m - 1]);
+console.log(dis[n - 1][m - 1]);
