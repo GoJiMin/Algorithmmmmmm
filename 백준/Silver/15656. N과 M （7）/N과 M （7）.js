9f453e1ef88831f2c4c7abd91376b88a8ff2f3d4
Prolip <gojimin3095@gmail.com>
2024-09-01 22:55:32 +0900

    [Silver III] Title: N과 M (7), Time: 880 ms, Memory: 175332 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.js
@@ -0,0 +1,29 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const [nm, numbers] = input;
+
+const [n, m] = nm.split(" ");
+const nums = numbers.split(" ").sort((a, b) => a - b);
+
+const arr = [];
+
+let result = "";
+
+function solution(k) {
+  if (k === +m) {
+    return (result += arr.join(" ") + "\n");
+  }
+
+  for (let i = 0; i < +n; i++) {
+    arr[k] = nums[i];
+    solution(k + 1);
+  }
+}
+
+solution(0);
+
+console.log(result);
