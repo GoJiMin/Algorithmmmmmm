480b46fed483c1ed98a452670638144a19f34ac4
Prolip <gojimin3095@gmail.com>
2024-09-11 21:33:26 +0900

    [Silver I] Title: 안전 영역, Time: 328 ms, Memory: 17968 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.js
+++ 백준/Silver/2468. 안전 영역/안전 영역.js
@@ -49,41 +49,43 @@ class Queue {
 }
 
 const [N, ...arr] = input;
+
 const n = Number(N);
 let maxHeight = 0;
-const graph = arr.map((el) =>
-  el
-    .trim()
-    .split(" ")
-    .map((s) => {
-      // 문제에서 제공되는 각 지역의 높이는 1이상 100이하.
-      // 이렇게 maxHeight을 미리 상정하고 반복문을 돌리면 높이가 3일 때 쓸모없이 100까지 돌릴 필요가 없음.
-      const curHeight = Number(s);
 
-      maxHeight = maxHeight < curHeight ? curHeight : maxHeight;
+const graph = arr.map((el) => el.trim().split(" ").map((s) => {//
+  const curHeight = Number(s); //
 
-      return curHeight;
-    })
-);
+  if(curHeight > maxHeight) maxHeight = curHeight; //
 
-// 여기서 vis를 초기에 -1로 설정한 뒤 최대 높이까지 반복문을 순회하며 각 좌표를 해당 높이로 설정함.
-// 이는 최대 높이까지의 배열을 계속 선언하지 않고도 사용하기 위함임.
+  return curHeight
+}));
+
 const vis = Array.from(Array(n), () => Array(n).fill(-1));
 
 const dx = [0, 0, -1, 1];
 const dy = [1, -1, 0, 0];
 
-let result = 0;
 
-const queue = new Queue();
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < n; j++) {
+//     if (maxHeight < graph[i][j]) {
+//       maxHeight = graph[i][j];
+//     }
+//   }
+// }
 
-for (let i = 0; i < maxHeight; i++) {
+let maxArea = 0;
+
+function bfs(curHeight) {
+  const queue = new Queue();
   let area = 0;
-  for (let j = 0; j < n; j++) {
-    for (let k = 0; k < n; k++) {
-      if (vis[j][k] < i && graph[j][k] > i) {
-        queue.push([j, k]);
-        vis[j][k] = i;
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (graph[i][j] > curHeight && vis[i][j] < curHeight) {
+        queue.push([i, j]);
+        vis[i][j] = curHeight;
         area++;
 
         while (!queue.empty()) {
@@ -94,17 +96,22 @@ for (let i = 0; i < maxHeight; i++) {
             const ny = y + dy[dir];
 
             if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-            if (graph[nx][ny] <= i || vis[nx][ny] >= i) continue;
+            if (curHeight >= graph[nx][ny] || vis[nx][ny] >= curHeight)
+              continue;
 
             queue.push([nx, ny]);
-            vis[nx][ny] = i;
+            vis[nx][ny] = curHeight;
           }
         }
       }
     }
   }
 
-  result = area > result ? area : result;
+  if (maxArea < area) maxArea = area;
 }
 
-console.log(result);
+for (let i = 0; i < maxHeight; i++) {
+  bfs(i);
+}
+
+console.log(maxArea);
