cbdc0e8c08c16f34746344d367e40c1cd95cc01d
Prolip <gojimin3095@gmail.com>
2024-11-17 02:08:08 +0900

    [Bronze V] Title: A+B - 7, Time: 100 ms, Memory: 9536 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,11 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+for (let i = 1; i <= +input[0]; i++) {
+  const [a, b] = input[i].split(" ").map(Number);
+
+  console.log(`Case #${i}: ${a + b}`);
+}
