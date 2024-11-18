d232109e883bcbb85f2ecb5020e4262310bcc30e
Prolip <gojimin3095@gmail.com>
2024-11-19 00:32:41 +0900

    [Silver V] Title: 너의 평점은, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.js
@@ -0,0 +1,33 @@
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n");
+
+const GRADES = {
+  "A+": 4.5,
+  A0: 4.0,
+  "B+": 3.5,
+  B0: 3.0,
+  "C+": 2.5,
+  C0: 2.0,
+  "D+": 1.5,
+  D0: 1.0,
+  F: 0.0,
+};
+
+let prefix = 0;
+let total = 0;
+
+for (let i = 0; i < input.length; i++) {
+  const [_, score, grade] = input[i].trim().split(" ");
+
+  if (grade === "P") {
+    continue;
+  } else {
+    prefix += Number(score * GRADES[grade]);
+    total += parseInt(score);
+  }
+}
+
+console.log((prefix / total).toFixed(6));
