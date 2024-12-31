9732c1706f93013b31c7932c2a0c06ebc6fbbfe1
Prolip <gojimin3095@gmail.com>
2024-12-31 23:51:11 +0900

    [Silver III] Title: 발머의 피크 이론, Time: 364 ms, Memory: 63868 KB -BaekjoonHub

--- 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.js
+++ 백준/Silver/27496. 발머의 피크 이론/발머의 피크 이론.js
@@ -16,22 +16,51 @@ const input = require("fs")
  * prefix[i] - prefix[i - l] 이런식으로 구하면 되겠네요.
  */
 
+// const [n, l] = input[0].split(" ").map(Number);
+// const arr = input[1].split(" ").map(Number);
+
+// for (let i = 1; i < n; i++) {
+//   arr[i] = arr[i - 1] + arr[i];
+// }
+
+// let cnt = 0;
+
+// for (let i = 0; i < n; i++) {
+//   const cur = arr[i] - (arr[i - l] || 0);
+//   const concentration = cur * 0.001;
+
+//   if (concentration >= 0.129 && concentration <= 0.138) {
+//     cnt++;
+//   }
+// }
+
+// console.log(cnt);
+
+/**
+ * 위에까지가 누적합 개념...이고,, 이번엔 슬라이딩 윈도우로 풀어봅시다.
+ * 근데 참 이름이 거창하네요.. 어쨌든! 슬라이딩 윈도우를 사용하면 별도의 누적합 배열을 구해놓지 않으니
+ * 메모리를 덜 쓰겠죠? 그냥 초기 값 구해놓고 옆으로 밀어버립시다.
+ */
+
 const [n, l] = input[0].split(" ").map(Number);
 const arr = input[1].split(" ").map(Number);
 
-for (let i = 1; i < n; i++) {
-  arr[i] = arr[i - 1] + arr[i];
-}
-
+let sum = 0; // 최초의 혈중 알코올 농도는 0이라고 문제에 써있으니까..
 let cnt = 0;
 
-for (let i = 0; i < n; i++) {
-  const cur = arr[i] - (arr[i - l] || 0);
-  const concentration = cur * 0.001;
+// l 만큼의 지속시간이니까 초기의 윈도우?는 l만큼 더해놓으면 되겠죠?
+for (let i = 0; i < l; i++) {
+  sum += arr[i];
 
-  if (concentration >= 0.129 && concentration <= 0.138) {
-    cnt++;
-  }
+  if (sum >= 129 && sum <= 138) cnt++;
+}
+
+for (let i = l; i < n; i++) {
+  // 오른쪽으로 밀어야되니까..
+  sum += arr[i];
+  sum -= arr[i - l];
+
+  if (sum >= 129 && sum <= 138) cnt++;
 }
 
 console.log(cnt);
