d619b2f3a30da90fc8ddd2ef9d9393b6524871ed
Prolip <gojimin3095@gmail.com>
2025-01-25 17:08:28 +0900

    [Silver IV] Title: 수들의 합 2, Time: 156 ms, Memory: 9776 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.js
@@ -4,26 +4,24 @@ const input = require("fs")
   .trim()
   .split("\n");
 
-const [n, s] = input[0].split(" ").map(Number);
+const [n, m] = input[0].split(" ").map(Number);
 const arr = input[1].split(" ").map(Number);
 
-for (let i = 1; i < n; i++) {
-  arr[i] = arr[i] + arr[i - 1];
-}
+let en = 0;
+let tot = arr[0];
 
-/**
- * 1 3 6 10 12 17 20 21 22 24 일 때,
- *
- * 3부터 4까지의 구간합을 구하고 싶다면, prefix[4] - prefix[2] 12 - 6 => 4 + 2 구간..
- */
-let ans = 0;
-for (let i = 0; i < n; i++) {
-  // 구간을 모두 돌면서, s 값에 해당하는 구간을 찾으면 ans를 증가.
-  if (arr[i] === s) ans++;
+let cnt = 0;
+for (let st = 0; st < n; st++) {
+  while (en < n && tot < m) {
+    en++;
 
-  for (let j = 0; j < i; j++) {
-    if (arr[i] - arr[j] === s) ans++;
+    if (en !== n) tot += arr[en];
   }
+
+  if (en === n) break;
+  if (tot === m) cnt++;
+
+  tot -= arr[st];
 }
 
-console.log(ans);
+console.log(cnt);
