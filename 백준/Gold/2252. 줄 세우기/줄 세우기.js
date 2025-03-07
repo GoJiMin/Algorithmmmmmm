a7c7909402a66d4f586a6bb846b1622022f487af
Prolip <gojimin3095@gmail.com>
2025-03-07 14:03:39 +0900

    [Gold III] Title: 줄 세우기, Time: 360 ms, Memory: 42972 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.js
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.js
@@ -7,30 +7,31 @@ const input = require("fs")
 const [n, m] = input[0].split(" ").map(Number);
 
 const adj = Array.from({ length: n + 1 }, () => []);
+const inDegree = Array(n + 1).fill(0);
 
 for (let i = 1; i <= m; i++) {
   const [u, v] = input[i].split(" ").map(Number);
 
   adj[u].push(v);
+  inDegree[v]++;
 }
 
-const vis = Array(n + 1).fill(false);
+const queue = [];
+
+for (let i = 1; i <= n; i++) {
+  if (inDegree[i] === 0) queue.push(i);
+}
 
 const result = [];
 
-function dfs(v) {
-  vis[v] = true;
-  for (const nxt of adj[v]) {
-    if (!vis[nxt]) dfs(nxt);
+while (queue.length) {
+  const cur = queue.shift();
+
+  result.push(cur);
+
+  for (const nxt of adj[cur]) {
+    if (--inDegree[nxt] === 0) queue.push(nxt);
   }
-
-  result.push(v);
 }
 
-for (let i = 1; i <= n; i++) {
-  if (!vis[i]) dfs(i);
-}
-
-console.log(result.reverse().join(" "));
-
-console.log(adj);
+console.log(result.join(" "));
