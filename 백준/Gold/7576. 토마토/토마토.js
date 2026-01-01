36be49c6f0ca3581847c83c96f0fd90eff2fe96f
Prolip <gojimin3095@gmail.com>
2026-01-01 22:35:10 +0900

    [Gold V] Title: 토마토, Time: 596 ms, Memory: 162068 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.js
+++ 백준/Gold/7576. 토마토/토마토.js
@@ -1,100 +1,92 @@
-const input = require("fs")
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
   .toString()
   .trim()
-  .split("\n");
+  .split('\n');
 
-const [nm, ...arr] = input;
+/**
+ * 익은 토마토(1)가 있다면 주위에 익지 않은 토마토(0)가 전염 당한다..!
+ * 하지만 -1은 벽이기 때문에 갈 수 없다..!!
+ *
+ * 사짜 큐로 풀어버려
+ */
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+const [m, n] = input[0].split(' ').map(Number);
 
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
+const board = [];
+for (let i = 1; i <= n; i++) board.push(input[i].trim().split(' '));
 
-  push(data) {
-    const node = new Node(data);
+const dirs = [
+  [1, 0],
+  [-1, 0],
+  [0, -1],
+  [0, 1],
+];
 
-    if (!this.head) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.tail.next = node;
-      this.tail = node;
-    }
+const dist = Array.from({length: n}, () => Array(m).fill(-1));
 
-    this.length++;
-  }
+/**
+ * 먼저 배열을 모두 순회해서 토마토의 좌표를 모두 큐에 넣어버립시다..
+ *
+ * 한 사이클마다 토마토들을 전염시켜야 하기 때문..! 큐는 FIFO잖아요? 먼저 들어간 토마토가 먼저 나옵니다.
+ * 벽은 통과할 수 없기 때문에 벽에 대한 적절한 처리도 필요합니다.
+ */
 
-  pop() {
-    if (!this.head) return;
-
-    const data = this.head.data;
-
-    this.head = this.head.next;
-    this.length--;
-
-    return data;
-  }
-
-  empty() {
-    return this.length === 0;
-  }
-}
-
-const [n, m] = nm.split(" ").map(Number);
-const box = arr.map((el) => el.trim().split(" "));
-
-const dx = [0, 0, -1, 1];
-const dy = [1, -1, 0, 0];
-
-const queue = new Queue();
-const dist = Array.from(Array(m), () => Array(n).fill(0));
-
-for (let i = 0; i < m; i++) {
-  for (let j = 0; j < n; j++) {
-    if (box[i][j] === "1") {
+const queue = [];
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
+    if (board[i][j] === '-1') {
+      dist[i][j] = 0; // 통과 못 하게 벽은 방문 표시해놓기..
+    } else if (board[i][j] === '1') {
       queue.push([i, j]);
       dist[i][j] = 0;
     }
-    if (box[i][j] === "0") {
-      dist[i][j] = -1;
+  }
+}
+
+let head = 0;
+while (head !== queue.length) {
+  const [x, y] = queue[head++];
+
+  for (const [dx, dy] of dirs) {
+    const nx = x + dx;
+    const ny = y + dy;
+
+    if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+    if (dist[nx][ny] === -1) {
+      queue.push([nx, ny]);
+      dist[nx][ny] = dist[x][y] + 1;
     }
   }
 }
 
-while (!queue.empty()) {
-  const [x, y] = queue.pop();
+/**
+ * 다 돌렸으면 dist 배열에 토마토가 전염되는데 걸리는 거리가 모두 담겨있겠죠?
+ *
+ * 만약 벽에 가로막힌 토마토가 있다면 아래와 같이 나올 거 같은데용
+ * -1 0 1
+ * 0 1 1
+ * 1 1 1
+ *
+ * 그럼 -1이 있다면 -1을 출력하고 그게 아니라면 최대값을 출력하면 되겠죠?
+ */
 
-  for (let dir = 0; dir < 4; dir++) {
-    const nx = x + dx[dir];
-    const ny = y + dy[dir];
-
-    if (nx < 0 || nx >= m || ny < 0 || ny >= n) continue;
-    if (dist[nx][ny] >= 0) continue;
-    queue.push([nx, ny]);
-    dist[nx][ny] = dist[x][y] + 1;
-  }
-}
-
-let ans = 0;
-for (let i = 0; i < m; i++) {
-  for (let j = 0; j < n; j++) {
+let max = 0;
+for (let i = 0; i < n; i++) {
+  for (let j = 0; j < m; j++) {
     if (dist[i][j] === -1) {
       console.log(-1);
-      return;
+      process.exit(0); // 그냥 -1 출력하고 종료 시그널 보내버리죠
+    } else {
+      max = Math.max(dist[i][j], max);
     }
-
-    ans = Math.max(ans, dist[i][j]);
   }
 }
 
-console.log(ans);
+/**
+ * 아니 2중 for문을 2번이나 돌리다니!! 라고 생각한 당신!
+ *
+ * 대충 시간제한 1초면 1억번 정도 돌아가야 하는데, 지금 N과 M의 최대값이 1000이죵?
+ * 1000000 * 2니까 대충 시간 복잡도는 문제 없습니다.
+ */
+console.log(max);
