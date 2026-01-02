cb93d940ea74630cdd1cd2ced55cc454ac871a32
Prolip <gojimin3095@gmail.com>
2026-01-02 14:17:29 +0900

    [Silver I] Title: 숨바꼭질, Time: 172 ms, Memory: 16212 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.js
@@ -1,74 +1,35 @@
-const input = require("fs")
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
   .toString()
-  .trim()
-  .split(" ")
-  .map(Number);
+  .trim();
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+const [n, k] = input.split(' ').map(Number);
 
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
+/**
+ * n은 수빈이가 위치한 지점..
+ * k는 동생이 위치한 지점.. target..
+ *
+ * 수빈이가 움직일 수 있는 방법은 현재 위치 X일 때 x - 1, x + 1, x * 2..
+ */
 
-  push(data) {
-    const node = new Node(data);
+// 점의 최대값은 100_000..
+const dist = Array(100_001).fill(-1);
 
-    if (!this.head) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.tail.next = node;
-      this.tail = node;
-    }
-
-    this.length++;
-  }
-
-  pop() {
-    if (!this.head) return;
-
-    const data = this.head.data;
-
-    this.head = this.head.next;
-    this.length--;
-
-    return data;
-  }
-
-  empty() {
-    return this.length === 0;
-  }
-}
-
-const [n, k] = input;
-
-const dist = Array(100002).fill(-1);
-const queue = new Queue();
-
+const queue = [n];
 dist[n] = 0;
-queue.push(n);
 
-// k에 도달해 k의 dist 값이 바뀐다면 while문을 종료함.
+let head = 0;
+// K의 거리값이 변경된다면 종료..
 while (dist[k] === -1) {
-  const cur = queue.pop();
+  const x = queue[head++];
 
-  // 수빈이가 움직일 수 있는 경우를 뽑아서 씀.
-  for (let act of [cur + 1, cur - 1, cur * 2]) {
-    // 지정된 범위를 벗어나면 넘어감.
-    if (act < 0 || act > 100000) continue;
-    // 이미 도달한 장소라면 넘어감.
-    if (dist[act] !== -1) continue;
-    dist[act] = dist[cur] + 1;
-    queue.push(act);
+  for (const nx of [x - 1, x + 1, x * 2]) {
+    // 점의 범위를 벗어난다면 패스..
+    if (nx < 0 || nx > 100_000) continue;
+    if (dist[nx] === -1) {
+      dist[nx] = dist[x] + 1;
+      queue.push(nx);
+    }
   }
 }
 
