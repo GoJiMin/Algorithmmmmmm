396adbecca77159f1fa237814b895ca4b42f041b
Prolip <gojimin3095@gmail.com>
2026-01-04 19:14:58 +0900

    [Silver I] Title: 나이트의 이동, Time: 600 ms, Memory: 89604 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.js
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.js
@@ -1,87 +1,70 @@
-const input = require("fs")
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+const input = require('fs')
+  .readFileSync(process.platform === 'linux' ? '/dev/stdin' : 'input.txt')
   .toString()
   .trim()
-  .split("\n");
+  .split('\n');
 
-class Node {
-  constructor(data) {
-    this.data = data;
-    this.next = null;
-  }
-}
+/**
+ * 나이트가 이동 가능한 범위는 총 8개..
+ *
+ * 정해진 칸으로 이동하는데 총 몇 번 움직여야 하는지..
+ *
+ * [x, y] 좌표일 때 나이트는
+ * [x + 1, y + 2],
+ * [x + 2, y + 1],
+ * [x - 1, y + 2]
+ * [x - 2, y + 1],
+ * [x + 1, y - 2],
+ * [x + 2, y - 1],
+ * [x - 1, y - 2],
+ * [x - 2, y - 1]
+ *
+ * 로 이동이 가능..
+ */
 
-class Queue {
-  constructor() {
-    this.head = null;
-    this.tail = null;
-    this.length = 0;
-  }
+const dirs = [
+  [1, 2],
+  [2, 1],
+  [-1, 2],
+  [-2, 1],
+  [1, -2],
+  [2, -1],
+  [-1, -2],
+  [-2, -1],
+];
 
-  push(data) {
-    const node = new Node(data);
+let idx = 0;
+const T = Number(input[idx++]);
 
-    if (!this.head) {
-      this.head = node;
-      this.tail = node;
-    } else {
-      this.tail.next = node;
-      this.tail = node;
-    }
+const result = [];
+for (let t = 0; t < T; t++) {
+  const l = Number(input[idx++]);
 
-    this.length++;
-  }
+  const [startX, startY] = input[idx++].split(' ').map(Number);
+  const [targetX, targetY] = input[idx++].split(' ').map(Number);
 
-  pop() {
-    if (!this.head) return;
+  const dist = Array.from({length: l}, () => Array(l).fill(-1));
 
-    const data = this.head.data;
+  const queue = [[startX, startY]];
+  dist[startX][startY] = 0;
 
-    this.head = this.head.next;
-    this.length--;
+  let head = 0;
+  while (dist[targetX][targetY] === -1) {
+    const [x, y] = queue[head++];
 
-    return data;
-  }
+    for (const [dx, dy] of dirs) {
+      const nx = x + dx;
+      const ny = y + dy;
 
-  empty() {
-    return this.length === 0;
-  }
-}
-
-const n = Number(input[0]);
-
-for (let i = 0; i < n; i++) {
-  const [I, ...arr] = input.splice(1, 3);
-  const loc = arr.map((el) => el.trim().split(" ").map(Number));
-
-  const vis = Array.from(Array(+I), () => Array(+I).fill(-1));
-  const queue = new Queue();
-
-  queue.push(loc[0]);
-  vis[loc[0][0]][loc[0][1]] = 0;
-
-  while (vis[loc[1][0]][loc[1][1]] === -1) {
-    const [x, y] = queue.pop();
-
-    for (let act of [
-      [x + 2, y + 1],
-      [x + 1, y + 2],
-      [x + 2, y - 1],
-      [x + 1, y - 2],
-      [x - 2, y + 1],
-      [x - 1, y + 2],
-      [x - 2, y - 1],
-      [x - 1, y - 2],
-    ]) {
-      const [nx, ny] = act;
-
-      if (nx < 0 || nx >= +I || ny < 0 || ny >= +I) continue;
-      if (vis[nx][ny] > 0) continue;
-
-      queue.push([nx, ny]);
-      vis[nx][ny] = vis[x][y] + 1;
+      if (nx < 0 || nx >= l || ny < 0 || ny >= l) continue;
+      if (dist[nx][ny] === -1) {
+        queue.push([nx, ny]);
+        dist[nx][ny] = dist[x][y] + 1;
+      }
     }
   }
 
-  console.log(vis[loc[1][0]][loc[1][1]]);
+  result.push(dist[targetX][targetY]);
 }
+
+console.log(result.join('\n'));
